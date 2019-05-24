9eb45db554de569957d3c385f8cc2e1c19501f8e
mcather <14895427+MattCatz@users.noreply.github.com>
2019-05-24 12:22:42 -0500

    Fixed serve and redirect

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
 
 function get_primitive() {
-	go get -u github.com/fogleman/primitive
+	go get -u -v github.com/fogleman/primitive
 	export PATH=$PATH:$GOPATH/bin/
 }
 
-get_primitive
-gulp
-bundle exec jekyll serve
+{ 
+	get_primitive
+	gulp
+	bundle exec jekyll build
+} 2>&1
