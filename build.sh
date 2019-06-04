cd0791011c703dc0c094dcd6b775495bb7cbe710
Matthew <14895427+MattCatz@users.noreply.github.com>
2019-06-03 17:43:33 -0700

    Trying to utilize the netify cache

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 
 function get_primitive() {
-	go get -u -v github.com/fogleman/primitive
+	go get -u github.com/fogleman/primitive
 	export PATH=$PATH:$GOPATH/bin/
 }
 
-{ 
+{
+	mkdir -p /opt/build/cache/sqip
 	get_primitive
 	npm install
 	./node_modules/gulp/bin/gulp.js
