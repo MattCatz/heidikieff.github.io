de940c70b44528830d13e309c5479c11098da401
Matthew <14895427+MattCatz@users.noreply.github.com>
2019-06-03 17:43:33 -0700

    Trying to utilize the netify cache

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { parallel, src, dest } = require('gulp');
+const { parallel, series, src, dest } = require('gulp');
 const responsive = require('gulp-responsive');
 const changed = require('gulp-changed');
 const sqip = require('gulp-sqip');
@@ -37,6 +37,8 @@ const webp_sizes = {
 const responsive_options = {
   errorOnEnlargement: false,
   errorOnUnusedConfig: false,
+  stats: false,
+  silent: true, 
 }
 
 const sqip_config = {
@@ -76,26 +78,33 @@ function convert_webp() {
 }
 
 function generate_placeholders_PNG() {
-  const DEST = '_site/assets/placeholders/';
+  const CACHE = '/opt/build/cache/sqip/';
   return src('assets/img/*.png')
-    .pipe(changed(DEST,{extension:'.svg'}))
+    .pipe(changed(CACHE,{extension:'.svg'}))
     .pipe(responsive({'*':{width:128}}, responsive_options))
     .pipe(sqip(sqip_config))
-    .pipe(dest(DEST));
+    .pipe(dest(CACHE));
 }
 
 function generate_placeholders_JPEG() {
-  const DEST = '_site/assets/placeholders/';
+  const CACHE = '/opt/build/cache/sqip/';
   return src('assets/img/*.jpeg')
-    .pipe(changed(DEST,{extension:'.svg'}))
+    .pipe(changed(CACHE,{extension:'.svg'}))
     .pipe(responsive({'*':{width:128}}, responsive_options))
     .pipe(sqip(sqip_config))
+    .pipe(dest(CACHE));
+}
+
+function get_from_cache() {
+  const CACHE = '/opt/build/cache/sqip/';
+  const DEST = '_site/assets/placeholders/';
+  return src(CACHE + '*.svg')
     .pipe(dest(DEST));
 }
 
 const resize_pictures = parallel(resize_png, resize_jpg);
 const convert_pictures = convert_webp;
-const generate_placeholders = parallel(generate_placeholders_PNG,generate_placeholders_JPEG);
+const generate_placeholders = series(parallel(generate_placeholders_PNG,generate_placeholders_JPEG), get_from_cache);
 
 exports.resize_pictures = resize_pictures;
 exports.convert_pictures = convert_pictures;
